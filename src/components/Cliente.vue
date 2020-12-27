aafe80033e45ff0eea400b741637c5216bf9543a
Mardonio Melo F <mardonio.quimico@gmail.com>
2020-12-27 11:37:50 -0300

    Update Project - Aula 09

---
+++ src/components/Cliente.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="cliente"> 
+        <input type="text" :value="nome">
+        <hr>
+        <input type="text" :value="desc">
+        <h2 id="cliente-nome">{{ nome }}</h2>
+        <p>Descrição do cliente: {{ desc }}</p>      
+    </div>  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            nome: "Mardônio Melo 123 ",
+            numero: 0,
+            email: "",
+            idade: 0,
+            desc: "Lorem ipsum 123"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #cliente{
+     max-width: 600px;
+     height: 120px;
+    }   
+</style>
